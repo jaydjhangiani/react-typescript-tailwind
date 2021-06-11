9b768ed8b2af2cf3a49d5dc5e05cba7a43068828
Jay Jhangiani <jaydjhangiani@gmail.com>
2021-06-12 01:43:03 +0530

    Added HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 import './App.css';
+import HomePage from './app/containers/HomePage';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hi</h1>
-    </div>
+    <AppContainer>
+     <HomePage/>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `}
+`;
